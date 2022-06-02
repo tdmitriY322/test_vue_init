d549614ae2a81b1f1fc4680ae86e765f7e5744d8
Dmitriy <dimon_op@mail.ru>
2022-06-02 19:15:16 +0900

    add toolbar

---
+++ src/components/Toolbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <v-toolbar elevation="4" flat rounded shaped>
+      <v-toolbar-title>Initkz</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <div>
+        <v-row d-flex align="center" justify="space-around">
+          <v-btn text to='/'> Слайдер </v-btn>
+          <v-btn text to='/currency'> Курс валют </v-btn>
+          <v-btn text to='/chat'> Чат </v-btn>
+        </v-row>
+      </div>
+      <v-spacer></v-spacer>
+      <div></div>
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    isDisabled: null
+  }),
+  methods: {
+  },
+};
+</script>
---
+++ src/views/layout/main.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-app id="inspire">
+        <Toolbar/>
+        <v-main style="background: #f5f5f540">
+        <v-container class="py-8 px-6" fluid>
+            <slot />
+        </v-container>
+        
+        </v-main>
+  </v-app>
+</template>
+
+
+<script>
+import Toolbar from '@/components/Toolbar' 
+export default {
+  name: "App",
+  components: { Toolbar },
+  data: () => ({
+  }),
+  methods: {},
+};
+</script>
