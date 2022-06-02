f7fd7a82672de0dbd3bd27fbb3673cf8f690c63e
Dmitriy <dimon_op@mail.ru>
2022-06-02 19:17:08 +0900

    change store

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,12 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import Rate from './modules/Rate';
+// vuex модули 
+Vue.use(Vuex);
 
-Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+    modules: {
+        Rate,
+    },
+});
---
+++ src/store/modules/Rate.js
@@ -0,0 +1,20 @@
+// import axios from "axios";
+export default {
+  name: 'Test',
+  namespaced: true,
+  state: {
+    currency: []
+  },
+
+  getters: {
+    RATE_STATE(state) {
+      return state;
+    },
+  },
+
+  mutations: {
+  },
+
+  actions: {
+  }
+};
