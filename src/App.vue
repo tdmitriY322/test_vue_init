bf09d0ddc43935d1f45dadcda960a780598ead0e
Dmitriy <dimon_op@mail.ru>
2022-06-02 19:16:04 +0900

    change router navigation

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,25 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <component
+  :is="layout"
+  >
+    <router-view />
+  </component>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import MainLayout from '@/views/layout/main';
+export default {
+  name: "App",
+  components: { MainLayout,},
+  data: () => ({
+  }),
+  computed: {
+    layout() {
+      console.log(this.$route);
+      return (this.$route.meta.layout || 'main' ) + '-layout'
+    },
+  },
+  methods: {},
+};
+</script>
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
